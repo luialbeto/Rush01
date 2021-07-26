5b0c779eeb7d6d6abe4867ad4cda2feac03230fd
Luiz Araujo <araujo.script@gmail.com>
2021-07-26 09:13:42 -0300

    Update ft_get_input.c

--- ex00/ft_get_input.c
+++ ex00/ft_get_input.c
@@ -13,7 +13,7 @@ t_input	ft_get_input(char *str)
 	input.left = (int *) malloc(4 * sizeof(int));
 	input.right = (int *) malloc(4 * sizeof(int));
 	i = 0;
-	while (*str != '\0')
+	while (*str != '\0') // guarda valor numérico na string correspondente
 	{
 		ft_store_values(str, &input, &i);
 		str++;
@@ -21,23 +21,23 @@ t_input	ft_get_input(char *str)
 	return (input);
 }
 
-void	ft_store_values(char *str, t_input *input, int *i)
+void	ft_store_values(char *str, t_input *input, int *i) // 
 {
 	int	value;
 	int	index;
 
 	if (*str != ' ')
 	{
-		value = *str - '0';
-		index = *i % 4;
+		value = *str - '0'; // transforma em int
+		index = *i % 4; // pega o valor do parâmetro i faz mod 4, pq o tabuleiro é 4x4
 		if (*i < 4)
-			input->up[index] = value;
+			input->up[index] = value; // coloca no vetor up
 		else if (*i < 8)
-			input->down[index] = value;
+			input->down[index] = value; // entre 5 e 8, guarda no vetor down
 		else if (*i < 12)
-			input->left[index] = value;
+			input->left[index] = value; // entre 9 e 12 guarda no left
 		else
-			input->right[index] = value;
+			input->right[index] = value; // caso contrário, no right
 		(*i)++;
 	}
 }
