2808304ede7d60920a3b0063e101742dbecd0a09
Luiz Araujo <araujo.script@gmail.com>
2021-07-26 09:17:13 -0300

    Update ft_create_matrix.c

--- ex00/ft_create_matrix.c
+++ ex00/ft_create_matrix.c
@@ -1,21 +1,21 @@
 #include <stdlib.h>
 #include "structs.h"
 
-t_matrix	ft_create_matrix(int rows, int cols)
+t_matrix	ft_create_matrix(int rows, int cols) // tipo t-matrix
 {
 	t_matrix	matrix;
 	int			i;
 	int			j;
 
 	i = 0;
-	matrix.rows = rows;
-	matrix.cols = cols;
-	matrix.values = (int **) malloc(rows * sizeof(int *));
-	while (i < rows)
+	matrix.rows = rows; // passamos a quantidade de linhas que essa matrix vai ter
+	matrix.cols = cols; // idem para colunas
+	matrix.values = (int **) malloc(rows * sizeof(int *)); // alocando um ponteiro para ponteior de int - matrix de duas dimensões - cada linha vai ter um vetor
+	while (i < rows) // percorre as linhas e aloca quantidade de colunas do tipo int para cada uma delas
 	{
 		matrix.values[i] = (int *) malloc(cols * sizeof(int));
 		j = 0;
-		while (j < cols)
+		while (j < cols) // enquanto j for menor que a coluna, percorre a coluna e coloca valor 0, para mostrar que não colocou nenhuma caixa na matrix
 		{
 			matrix.values[i][j] = 0;
 			j++;
