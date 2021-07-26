5f58e45c02caa4d53071b52143e83ff7562a61bd
Luiz Araujo <araujo.script@gmail.com>
2021-07-26 09:10:04 -0300

    Update main.c

--- ex00/main.c
+++ ex00/main.c
@@ -9,18 +9,18 @@ void		ft_print(t_matrix *m);
 void		ft_putstr(char *str, int size);
 void		ft_free_matrix(t_matrix *m);
 
-int	main(int argc, char **argv)
+int	main(int argc, char **argv) // quantidade e valores
 {
-	t_input		input;
+	t_input		input; // variável do tipo t_input - criada por nós - struct - facilitar e diminuir parâmetros - similar objeto em JS
 	t_matrix	matrix;
-	int			is_success;
+	int			is_success; // verifica se o programa inicia normalmente
 
-	is_success = 0;
-	if (argc > 1)
+	is_success = 0; // inicia como 0 - booleano e 0 é falso
+	if (argc > 1) // quando executa programa, quantidade de elementos padrão é 1
 	{
-		input = ft_get_input(argv[1]);
-		matrix = ft_create_matrix(4, 4);
-		is_success = ft_solve_rush(&input, &matrix);
+		input = ft_get_input(argv[1]); // chama a função ft_get_input, passando o valor 1 do vetor argv
+		matrix = ft_create_matrix(4, 4); // transforma valor em int
+		is_success = ft_solve_rush(&input, &matrix); 
 		if (is_success)
 			ft_print(&matrix);
 		ft_free_matrix(&matrix);
